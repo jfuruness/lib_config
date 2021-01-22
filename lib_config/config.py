315a45f2c5acad39f7815da3de11ff1af33c8066
Justin Furuness <jfuruness@gmail.com>
2021-01-21 20:41:08 -0500

    fixed config err

--- lib_config/config.py
+++ lib_config/config.py
@@ -10,14 +10,22 @@ __maintainer__ = "Justin Furuness"
 __email__ = "jfuruness@gmail.com"
 __status__ = "Development"
 
+import os
+
 from configparser import NoSectionError, ConfigParser as SCP
 
+from lib_utils import utils
+
 class Config:
-    def __init__(self, package):
+    def __init__(self, package="custom_config"):
         self.path = f"/etc/{package}.conf"
-        if not os.path.exist:
-            with open(self.path, "w+") as f:
-                pass
+        if not os.path.exists(self.path):
+            try:
+                with open(self.path, "w+") as f:
+                    pass
+            except PermissionError as e:
+                utils.run_cmds([f"sudo touch {self.path}",
+                                f"sudo chmod -R 777 {self.path}"])
     def write_section(self, section: str, kwargs: dict):
         _config = SCP()
         _config.read(self.path)
@@ -25,9 +33,16 @@ class Config:
         with open(self.path, "a") as f:
             _config.write(f)
 
-    def read_section_tag(self, section: str, tag: str):
+    def read(self, section: str, tag: str, raw: bool = True):
         try:
-            return SCP().read(self.path).get(section, tag, raw=raw)
-        except NoSectionError as e:
-            input("Fill in desired section, then press enter")
-            return self.read_section_tag(section, tag)
+            _conf = SCP()
+            _conf.read(self.path)
+            return _conf.get(section, tag, raw=raw)
+        except (NoSectionError, AttributeError) as e:
+            input(f"Fill in desired section in {self.path}, then press enter")
+            return self.read(section, tag)
+
+    def discord_creds(self):
+        """Returns email and password"""
+
+        return [self.read("Discord", tag) for tag in ["email", "password"]]
